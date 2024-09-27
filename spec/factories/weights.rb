439f7fb3b419f36de68cf1873118c9a77ea7fb98
yuuuuuuuka <yuuuuka.02.29@gmail.com>
2024-09-27 19:09:33 +0900

    weightテーブル　テストコード実装

--- spec/factories/weights.rb
+++ spec/factories/weights.rb
@@ -1,4 +1,7 @@
 FactoryBot.define do
   factory :weight do
+    weight { 65 }
+    date { Faker::Date.backward(days: 14) }
+    association :user
   end
 end
--- spec/models/weight_spec.rb
+++ spec/models/weight_spec.rb
@@ -1,5 +1,33 @@
 require 'rails_helper'
 
 RSpec.describe Weight, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @weight = FactoryBot.build(:weight)
+  end
+
+  describe '体重記録機能' do
+    context '体重が記録できるとき' do
+      it 'すべての項目が正しく入力されている場合、ユーザーは有効である' do
+        expect(@weight).to be_valid
+      end
+    end
+    context '体重が記録できないとき' do
+      it '体重が空では保存できない' do
+        @weight.weight = nil
+        expect(@weight.valid?).to eq(false)
+        expect(@weight.errors.full_messages).to include('Weight 体重を入力してください')
+      end
+
+      it '日付が空では保存できない' do
+        @weight.date = nil
+        @weight.valid?
+        expect(@weight.errors.full_messages).to include('Date 日付を入力してください。')
+      end
+      it 'ユーザーと紐付いていなければ保存できない' do
+        @weight.user = nil
+        @weight.valid?
+        expect(@weight.errors.full_messages).to include('User ユーザーが必要です')
+      end
+    end
+  end
 end
